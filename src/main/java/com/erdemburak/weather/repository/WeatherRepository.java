5e0bd51d1d3298aa9c9f067ba754cf80daf72950
erdemburak <burakerdem6@hotmail.com>
2024-02-17 00:25:38 +0300

    WeatherRepository - findFirstByRequestedCityNameOrderByUpdatedTimeDesc

--- src/main/java/com/erdemburak/weather/repository/WeatherRepository.java
+++ src/main/java/com/erdemburak/weather/repository/WeatherRepository.java
@@ -3,5 +3,10 @@ package com.erdemburak.weather.repository;
 import com.erdemburak.weather.model.WeatherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WeatherRepository extends JpaRepository<WeatherEntity, String> {
+
+    Optional<WeatherEntity> findFirstByRequestedCityNameOrderByUpdatedTimeDesc(String city);
+
 }
