1a51d710bd656eb2e3f773ec552954b2a3b67f35
erdemburak <burakerdem6@hotmail.com>
2024-02-17 00:22:58 +0300

    WeatherDto - Convert from entity to dto

--- src/main/java/com/erdemburak/weather/dto/WeatherDto.java
+++ src/main/java/com/erdemburak/weather/dto/WeatherDto.java
@@ -1,8 +1,15 @@
 package com.erdemburak.weather.dto;
 
+import com.erdemburak.weather.model.WeatherEntity;
+
 public record WeatherDto (
         String cityName,
         String country,
         Integer temperature
 ) {
+
+    public static WeatherDto convert(WeatherEntity from){
+        return new WeatherDto(from.getCityName(),from.getCountry(),from.getTemperature());
+    }
+
 }
