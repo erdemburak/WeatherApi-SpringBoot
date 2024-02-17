33cfb55cb059c6e12392c5a5925fa739eab3d996
erdemburak <burakerdem6@hotmail.com>
2024-02-17 16:51:32 +0300

    WeatherDto - record updated

--- src/main/java/com/erdemburak/weather/dto/WeatherDto.java
+++ src/main/java/com/erdemburak/weather/dto/WeatherDto.java
@@ -1,15 +1,24 @@
 package com.erdemburak.weather.dto;
 
 import com.erdemburak.weather.model.WeatherEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 
 public record WeatherDto (
         String cityName,
         String country,
-        Integer temperature
+        Integer temperature,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+        LocalDateTime updatedTime
 ) {
 
     public static WeatherDto convert(WeatherEntity from){
-        return new WeatherDto(from.getCityName(),from.getCountry(),from.getTemperature());
+        return new WeatherDto(
+                from.getCityName(),
+                from.getCountry(),
+                from.getTemperature(),
+                from.getUpdatedTime());
     }
 
 }
